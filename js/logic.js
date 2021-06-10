aa765af16167d3ba974bf884b9c9d2b08a544f58
SURAJ JADHAV <69894599+JadhavSuraj7rk@users.noreply.github.com>
2021-06-10 12:54:39 +0530

    Desktop part completed.

---
+++ js/logic.js
@@ -0,0 +1,23 @@
+// const navMenu = document.getElementById("nav-menu"),
+//   toggleMenu = document.getElementById("nav-toggle"),
+//   closeMenu = document.getElementById("nav-close");
+
+
+// // show navbar(mobile)  
+// toggleMenu.addEventListener("click", () => {
+//   navMenu.classList.toggle("navbar-show-menu");
+// });
+
+// // close navbar(mobile)  
+// closeMenu.addEventListener("click", () => {
+//   navMenu.classList.remove("navbar-show-menu");
+// });
+
+// navbar links active 
+const navLink = document.querySelectorAll(".nav__link");
+function linkAction() {
+  navLink.forEach((n) => n.classList.remove("navbar-link-active"));
+  this.classList.add("navbar-link-active");
+  navMenu.classList.remove("navbar-show-menu");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
